6a48433497ac8ca8e7cab324772d44d23b6cc25e
Joalison Pereira <joalison.pereira@hotmail.com>
2019-01-04 12:37:40 -0200

    Updates

---
+++ precache-manifest.445bafeb1f00d92391b3d5eb73ab409c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-layout/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "1beb0a523070c386bc12",
+    "url": "/react-layout/static/css/main.c9a0fb32.chunk.css"
+  },
+  {
+    "revision": "6d513a43ed74d1e5093f",
+    "url": "/react-layout/static/js/1.6d513a43.chunk.js"
+  },
+  {
+    "revision": "1f74c9619118606c103e",
+    "url": "/react-layout/static/js/runtime~main.1f74c961.js"
+  },
+  {
+    "revision": "0b9235c3b9875ba7995fef1d5b4b287a",
+    "url": "/react-layout/static/media/profile.0b9235c3.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-layout/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "1beb0a523070c386bc12",
+    "url": "/react-layout/static/js/main.1beb0a52.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-layout/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-layout/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-layout/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8415d11bfc90dd6f030ad26c2e3039d2",
+    "url": "/react-layout/static/media/image.8415d11b.jpg"
+  },
+  {
+    "revision": "8629b8443db58953ced1758c08524218",
+    "url": "/react-layout/index.html"
+  }
+];
